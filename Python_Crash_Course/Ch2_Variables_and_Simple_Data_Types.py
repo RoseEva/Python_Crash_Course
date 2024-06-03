53de9e08b61f501b335b7b5874979262b28d8e02
RoseEva <92346229+RoseEva@users.noreply.github.com>
2024-06-03 18:19:39 +0000

    Pending changes exported from your codespace

---
+++ Python_Crash_Course/Ch2_Variables_and_Simple_Data_Types.py
@@ -0,0 +1,40 @@
+#Ch. 2: Variables and Simple Data Types
+
+#Newline
+print('Hello World')
+print('Hello\nWorld')
+
+#New Tab
+print('Hello\tWorld')
+
+#List
+animals = ['fox, tortoise, armadillo']
+print(animals[0])
+
+#Format List
+group_name = "elrat"
+message = f"Hello, we are {group_name}"
+print(message)
+
+group_name = "elrat"
+message = f"Hello, we are {group_name.title()}"
+print(message)
+
+group_name = "elrat"
+message = f"Hello, we are {group_name.upper()}"
+print(message)
+
+#Remove Whitespace
+
+task = ' monitoring '
+print(task.rstrip())
+print(task.lstrip())
+print(task.strip())
+
+website = 'https://county.org'
+print(website.removeprefix('https://'))
+print(website.removesuffix('.org'))
+
+#Permanently remove whitespace requires reassigning the variable name to stripped value
+task = task.strip()
+print(task)
---
+++ Python_Crash_Course/Ch3_Lists.py
@@ -0,0 +1,11 @@
+#Create List
+animals = ['fox', 'bobcat', 'snake', 'gopher tortoise']
+
+#Access elements from left of list
+print(animals[0].title())
+print(f"There is a {animals[0].upper()}!")
+print(animals[1])
+
+#Access elements from right of list
+print(animals[-1])
+print(animals[-2])
